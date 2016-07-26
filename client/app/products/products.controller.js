d0a36bade826202544e91d1c8912eacbba8e7091
Tiago Lima <tiago.jlima@yahoo.com.br>
2016-07-26 18:31:14 -0300

    insert CRUD in factory products

--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -2,5 +2,38 @@
 
 angular.module('meanshopApp')
   .controller('ProductsCtrl', function ($scope, Products) {
-    $scope.products = Products;
+    $scope.products = Products.query();
+  })
+
+  .controller('ProductViewCtrl', function ($scope, $state, $stateParams, Products){
+
+    $scope.product = Products.get({id: $stateParams.id});
+
+    $scope.deleteProduct = function(){
+      Products.delete($scope.product);
+      $state.go('products');
+    };
+
+  })
+
+  .controller('ProductNewCtrl', function($scope, $state, $stateParams, Products) {
+
+    $scope.product = {};
+
+    $scope.addProduct = function(product){
+      Products.create($scope.product);
+      $state.go('products');
+    };
+
+  })
+
+  .controller('ProductEditCtrl', function($scope, $state, $stateParams, Products) {
+
+    $scope.product = Product.get({id: $stateParams.id});
+
+    $scope.editProduct = function(product) {
+      Products.update($scope.product);
+      $state.go('products');
+    };
+
   });
--- client/app/products/products.service.js
+++ client/app/products/products.service.js
@@ -5,7 +5,9 @@ angular.module('meanshopApp')
     // Service logic
     // ...
 
-    var _products = [
+    var _last_id = 5;
+
+    var _example_products = [
       {
         _id: 1,
         price: 123.45,
@@ -39,5 +41,46 @@ angular.module('meanshopApp')
     ];
 
     // Public API here
-    return _products;
+    return {
+      query: function(){
+        return _example_products;
+      },
+
+      get: function(params){
+        var result = {};
+        angular.forEach(_example_products, function(product){
+          if(product._id == params.id){
+            return this.product = product;
+          }
+        }, result);
+        return result.product;
+      },
+
+      delete: function(params){
+        angular.forEach(_example_products, function(product, index){
+          if(product._id == params._id) {
+            console.log(product, index);
+            _example_products.splice(index, 1);
+            return;
+          }
+        });
+      },
+
+      create: function(product){
+        product.id = ++_last_id;
+        _example_products.push(product);
+      },
+
+      update: function(product){
+        var item = this.get(product);
+        if(!item) return false;
+
+        item.title = product.title;
+        item.price = product.price;
+        item.quantify = product.quantify;
+        item.description = product.description;
+
+        return true;
+      }
+    };
   });
