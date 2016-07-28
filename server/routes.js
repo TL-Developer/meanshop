b4c59af45600ab6faae2f010ffcf8b90893c2d7b
Tiago Lima <tiago.jlima@yahoo.com.br>
2016-07-28 08:01:01 -0300

    implement API Products and TDD

--- server/routes.js
+++ server/routes.js
@@ -9,7 +9,8 @@ import path from 'path';
 
 export default function(app) {
   // Insert routes below
-  app.use('/api/things', require('./api/thing'));
+  app.use('/api/products/', require('./api/products'));
+  app.use('/api/products', require('./api/products'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth').default);
